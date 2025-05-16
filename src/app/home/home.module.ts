dba4daca9aa676713660bc7ce446c0ba197e4fe0
garbbagee <romandiazmiguelignacio@gmail.com>
2025-05-16 15:32:30 -0400

    front sidebar and ...
    barra lateral y la barra inferior

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'; // 👈 AÑADIR CUSTOM_ELEMENTS_SCHEMA
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
