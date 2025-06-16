cea43cf6a839732bb4a72ab5bf7742febe7a2579
garbbagee <romandiazmiguelignacio@gmail.com>
2025-06-15 21:00:50 -0400

    color fix feed

--- src/app/pages/feed/feed.page.ts
+++ src/app/pages/feed/feed.page.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { IonicModule, MenuController } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-feed',
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
   templateUrl: './feed.page.html',
   styleUrls: ['./feed.page.scss'],
 })
 export class FeedPage {
+  userName = 'Miguel Román'; // ← cámbialo por el nombre real desde AuthService si quieres
+  get userInitial() {
+    return this.userName.charAt(0).toUpperCase();
+  }
+
   constructor(
     private router: Router,
     private authService: AuthService,
